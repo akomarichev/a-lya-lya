bd25d845c803d018cb79329aa4562538a41a2fa6
calmnessart@gmail.com <calmnessart@gmail.com@50c03ab4-44b6-b86e-4c7f-354454521ca0>
2012-06-10 19:39:55 +0000

    Added login.activity focus listeners

--- trunk/VK_Chat/src/vk/chat/LoginActivity.java
+++ trunk/VK_Chat/src/vk/chat/LoginActivity.java
@@ -1,14 +1,12 @@
 package vk.chat;
 
-import org.json.JSONObject;
-
 import vk.api.API;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,7 +16,7 @@ public class LoginActivity extends Activity {
 	API api;
 	Typeface tf;
 	
-	Button b;
+	Button b_login;
 	ImageView iv_logo;
 	ImageView iv_login;
 	ImageView iv_pass;
@@ -46,17 +44,41 @@ public class LoginActivity extends Activity {
 	}
 	
 	private void setupUI(){
-		b = (Button) findViewById(R.id.button1);
+		b_login = (Button) findViewById(R.id.button1);
 		iv_logo = (ImageView) findViewById(R.id.imageView1);
 		iv_login = (ImageView) findViewById(R.id.imageView2);
 		iv_pass = (ImageView) findViewById(R.id.imageView3);
 		et_login = (EditText) findViewById(R.id.editText1);
+		et_login.setOnFocusChangeListener(et_loginClick);
 		et_password = (EditText) findViewById(R.id.editText2);
+		et_password.setOnFocusChangeListener(et_passwordClick);
 	}
 	
 	private void setupFonts(){
-		b.setTypeface(tf);
+		b_login.setTypeface(tf);
 		et_login.setTypeface(tf);
 		et_password.setTypeface(tf);
 	}
+	
+	private OnFocusChangeListener et_loginClick=new View.OnFocusChangeListener(){
+		@Override
+	    public void onFocusChange(View v, boolean hasFocus) {
+	        if (hasFocus) {
+	            iv_login.setImageResource(R.drawable.login_phone_active);
+	        }
+	        else
+	        	iv_login.setImageResource(R.drawable.login_phone);
+	    }
+	};
+	
+	private OnFocusChangeListener et_passwordClick=new View.OnFocusChangeListener(){
+		@Override
+	    public void onFocusChange(View v, boolean hasFocus) {
+	        if (hasFocus) {
+	        	iv_pass.setImageResource(R.drawable.login_pass_active);
+	        }
+	        else
+	        	iv_pass.setImageResource(R.drawable.login_pass);
+	    }
+	};
 }
