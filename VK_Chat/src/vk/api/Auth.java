56390496487f74890db46364144691466b229832
calmnessart <calmnessart@gmail.com>
2012-06-20 07:39:40 +0000

    Added change profile photo, google maps, gallery, camera.

--- VK_Chat/src/vk/api/Auth.java
+++ VK_Chat/src/vk/api/Auth.java
@@ -1,7 +1,6 @@
 package vk.api;
 
 public class Auth {
-
     public static String getSettings(){
         int settings=1+2+4+8+16+32+64+128+1024+2048+4096+8192+65536+131072+262144;
         return Integer.toString(settings);
